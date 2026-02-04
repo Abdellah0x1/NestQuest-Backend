9e63ad82ee806b2e68fe2580c8d4aadf9f3e1982
Abdellah EL GHENNAMI <abdellahelghanami67@gmail.com>
2026-02-04 13:39:05 +0100

    editing CORS

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 //app middlewares
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'], 
+  origin: ['http://localhost:5173', 'http://localhost:3000','https://nest-quest-client.vercel.app/'], 
   credentials: true
 }));
 
